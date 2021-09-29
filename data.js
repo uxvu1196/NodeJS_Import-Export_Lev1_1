d933d78626f61c3cb40a7db4dd210e15bbf74655
uxvu1196 <uxvu1196@gmail.com>
2021-09-30 01:17:45 +0200

    task 1 finished

---
+++ data.js
@@ -0,0 +1,5 @@
+const arrayNumbers = [12, 11, 1, 111, 144, 1355, 135]
+const arrayCars = ['bmw', 'audi', 'fiat', 'ferrari', 'opel']
+
+console.log(arrayCars)
+console.log(arrayNumbers)
---
+++ index.js
@@ -0,0 +1,4 @@
+// Lev1_1.29.09.2021
+
+let importFun = require("./function.js")
+let importData = require("./data.js")
